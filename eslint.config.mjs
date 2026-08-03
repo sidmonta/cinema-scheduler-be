c36a28e19ce0bd884c1fd3e05d58f6cfe476d7bb
Alessio Petrosino <alessiopetrosino.ap@gmail.com>
2026-08-03 15:57:50 +0200

    configurazione progetto: run lint, run typecheck e run dev partono senza errori

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['dist/', 'node_modules/', 'drizzle/'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+];
