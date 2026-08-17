7aee73699eb231d8ed85b8594f8d36296365acb2
sidmonta <sidmonta.15@gmail.com>
2026-08-17 17:04:21 +0200

    Fix log middleware

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -1,8 +1,14 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from 'express';
 
-export const log = (req: Request, _res: Response, next: NextFunction) => {
-    console.log("Chiamata in entrata", req.url);
-    const start = performance.now();
-    next();
-    console.log("Chiamata conclusa", performance.now() - start);
-}
+export const log = (req: Request, res: Response, next: NextFunction) => {
+  const start = process.hrtime.bigint();
+  res.on('finish', () => {
+    const end = process.hrtime.bigint();
+    const durationMs = Number(end - start) / 1e6;
+
+    console.log(
+      `[metrics] ${req.method} ${req.route?.path ?? req.path} ${res.statusCode} - ${durationMs.toFixed(2)}ms`,
+    );
+  });
+  next();
+};
